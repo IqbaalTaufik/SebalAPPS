9c65e3dc4492153d9572ceea2552492cdf1f0bb5
iqbaalt07 <pudge.warcraft3@gmail.com>
2020-05-15 20:30:24 +0700

    teuing lieur geus jangar

---
+++ app/src/main/java/com/example/mainidle/frenlis.kt
@@ -0,0 +1,12 @@
+package com.example.mainidle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class frenlis : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_frenlis)
+    }
+}
